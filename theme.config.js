c000d84c9b0ae845d8fa83d3a6ede45a47fc6377
Stas SUȘCOV <stas@nerd.ro>
2022-05-19 15:55:49 +0100

    Added the logo.

--- theme.config.js
+++ theme.config.js
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import logo from './static/velodrome.svg';
+
 export default {
   projectLink: 'https://github.com/velodrome-finance/docs/',
   docsRepositoryBase: 'https://github.com/velodrome-finance/docs/blob/master',
@@ -12,7 +15,8 @@ export default {
   footerEditLink: `Edit this page on GitHub`,
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Velodrome</span>
+      <Image src={logo} alt="Velodrome Finance" width={50} height={30} />
+      <span className="mr-2 font-extrabold md:inline">Velodrome Finance</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         Next-gen AMM on Optimism. Built on the revolutionary vision of Solidly.
       </span>
