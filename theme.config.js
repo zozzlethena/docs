3daca490f2e62fcb8253f8e6ea088d300285648f
Stas SUȘCOV <stas@nerd.ro>
2022-05-23 20:39:12 +0100

    Added brand assets/styling.

--- theme.config.js
+++ theme.config.js
@@ -1,5 +1,5 @@
 export default {
-  projectLink: 'https://github.com/velodrome-finance/docs/',
+  projectLink: 'https://github.com/velodrome-finance/',
   docsRepositoryBase: 'https://github.com/velodrome-finance/docs/blob/master',
   titleSuffix: ' – Velodrome Finance',
   nextLinks: true,
@@ -7,6 +7,7 @@ export default {
   search: false,
   unstable_stork: false,
   darkMode: true,
+  font: false,
   footer: true,
   footerText: `MIT ${new Date().getFullYear()} © Velodrome Finance.`,
   footerEditLink: `Edit this page on GitHub`,
@@ -15,12 +16,28 @@ export default {
       <img src="/velodrome.svg" alt="Velodrome Finance" style={{height: '30px', padding: '0 5px'}} />
       <span className="mr-2 font-extrabold md:inline">Velodrome Finance</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        Next-gen AMM on Optimism. Built on the revolutionary vision of Solidly.
+        The liquidity base-layer of the Optimism ecosystem.
       </span>
     </>
   ),
   head: (
     <>
+      <meta charset="utf-8" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Velodrome Finance: The liquidity base-layer of the Optimism ecosystem." />
+      <meta name="theme-color" content="#ffffff" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@VelodromeFi" />
+      <meta name="twitter:title" content="Velodrome Finance: The liquidity base-layer of the Optimism ecosystem." />
+      <meta name="twitter:description" content="Velodrome Finance is an automated market maker (AMM or simply, decentralized exchange) on Optimism L2 blockchain." />
+      <meta name="twitter:image" content="https://velodrome-docs.pages.dev/velodrome_banner.jpg" />
+
+      <meta property="og:title" content="Velodrome Finance: The liquidity base-layer of the Optimism ecosystem." />
+      <meta property="og:description" content="Velodrome Finance is an automated market maker (AMM or simply, decentralized exchange) on Optimism L2 blockchain." />
+      <meta name="apple-mobile-web-app-title" content="Velodrome Finance" />
+      <link rel="stylesheet" href="/fonts.css" />
     </>
   )
 }
